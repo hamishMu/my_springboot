8a02fc08e115ea08d732c9e560412c293fc0d862
hamishmu <731605632@qq.com>
2020-02-02 23:02:41 +0800

    add index.html

---
+++ src/main/java/com/hy/springboot03/demo/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.hy.springboot03.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
